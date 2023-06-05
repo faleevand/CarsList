2d33fd34286e2e9eee4586cfabe6cc78b95dc2ea
faleevand <faleevandre@gmail.com>
2023-06-05 22:39:35 +0300

    1

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,23 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.CarService.CarService;
+
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping()
+    public String getCarsByCount(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+        model.addAttribute("carscount", carService.getCarsByCount(count));
+        return "carscount";
+    }
+}
